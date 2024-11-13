5e8308fd7f54ba19a0694f0851bc8e13ae99b7d8
João Paulo <assislearn@gmail.com>
2024-11-13 17:46:23 -0300

    Project created

---
+++ lslist/src/main/java/com/jotaassis/lslist/LslistApplication.java
@@ -0,0 +1,13 @@
+package com.jotaassis.lslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LslistApplication.class, args);
+	}
+
+}
---
+++ lslist/src/test/java/com/jotaassis/lslist/LslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jotaassis.lslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
